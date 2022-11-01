15f7cf8937153469c48927e6debd7b26029e7f10
RyanShue <43621717+RyanShue@users.noreply.github.com>
2022-10-31 20:37:59 -0400

    Working code for inserting into MySQL database

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Axios from 'axios';
 
 function App() {
+  const [tutor, setTutor] = useState("")
+  const [subject, setSubject] = useState("")
+
+  const addTutor = () => {
+    Axios.post('http://localhost:3001/test', {
+      tutor: tutor,
+      subject: subject,   
+    })
+    .then(() => {
+      console.log('success')
+    });
+  }
+
   return (
     <div className="App">
         <h1>BoilerTutor</h1>
 
         <div className="form">
           <label>Tutor name:</label>
-          <input type="text" name="tutor"/>
+          <input type="text" name="tutor" onChange={(event) => setTutor(event.target.value)}/>
 
           <label>Subject:</label>
-          <input type="text" name="subject"/>
+          <input type="text" name="subject" onChange={(event) => setSubject(event.target.value)}/>
 
-          <button>Submit</button>
+          <button onClick={addTutor}>Submit</button>
         </div>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,10 +1,44 @@
-const express = require("express");
-const app = express();
+const express = require("express")
+const app = express()
+const mysql = require('mysql')
+const cors = require("cors")
+
+app.use(function (request, response, next) {
+    response.header("Access-Control-Allow-Origin", "*");
+    response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.use(express.json());
+
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: 'password',
+    database: 'testdb'
+})
+
+app.post('/test', (req, res) => {
+    //console.log(req.body)
+
+    const tutor = req.body.tutor;
+    const subject = req.body.subject;
+
+    db.query('INSERT INTO tutor (name, subject) VALUES (?, ?)', 
+    [tutor, subject],
+    (err, result) => {
+        if (err) {
+            console.log(err)
+        } else {
+            res.send("Values inserted successfully")
+        }
+    }
+    );
+});
 
 app.get("/", (req, res) => {
     res.send("hello world")
 });
 
 app.listen(3001, () => {
-    console.log("running on port 3001");
+    console.log("Server initialized on port 3001");
 });
