814b9d945c77788dcba4d3ad1c42a50f84b0f2f9
maitriparikh <mparikh159@gmail.com>
2022-12-07 23:20:53 -0500

    nav bar page + skeleton

---
+++ client/src/pages/navbar.js
@@ -0,0 +1,12 @@
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+
+<AppBar position="static">
+  <Toolbar variant="dense">
+    <IconButton edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
+      <MenuIcon />
+    </IconButton>
+  </Toolbar>
+</AppBar>
