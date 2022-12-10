e9670038f33d3ad8d84a4e136e497241103d61e4
Qusai Ghabrah <qghabrah@purdue.edu>
2022-12-10 01:36:52 -0500

    Implemented SessionStorage to store user info

--- server/index.js
+++ server/index.js
@@ -1,26 +1,18 @@
 const express = require("express");
 const app = express();
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path');
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
 
 const PORT = process.env.PORT || 3001;
 
 const db = mysql.createConnection({
-    host: '35.202.200.76',
-    user: 'root',
-    password: 'ZzLDQJgh93WcUk9yqbBgmSfSYspzyA',
-    database: 'BoilerTutor' 
+  host: "35.202.200.76",
+  user: "root",
+  password: "ZzLDQJgh93WcUk9yqbBgmSfSYspzyA",
+  database: "BoilerTutor",
 });
-
-/*app.get("/", (req, res) => {
-    const sqlInsert = "INSERT INTO Student (firstName, lastName, classStanding, emailAddress, password) VALUES ('Harry', 'Potter', 'Grad Student', 'harry@email.com', watermelonSugar);"
-    db.query(sqlInsert, (err, result) => {
-        res.send("hello worldz");
-    })
-    res.send("hello");
-});*/
 
 app.use(cors());
 
@@ -28,89 +20,109 @@ app.use(express.json());
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+/* Get user info + role (Student/Tutor) */
+app.get("/api/auth", (req, res) => {
+  const currEmail = req.query.email;
+  const CurrPassword = req.query.password;
+  console.log(currEmail, CurrPassword);
+  console.log(req.query);
+
+  const sqlGet =
+    "Select * from (SELECT *, 'Student' as role FROM Student union select *, 'Tutor' as role from Tutor) as users where email = ? and password = ?";
+  db.query(sqlGet, [currEmail, CurrPassword], (err, result) => {
+    res.send(result);
+  });
+});
 
 app.get("/api/get", (req, res) => {
-    const sqlSelect = "SELECT * FROM Student;";
-    db.query(sqlSelect, (err, result) => {
-        res.send(result);
-    });
+  const sqlSelect = "SELECT * FROM Student;";
+  db.query(sqlSelect, (err, result) => {
+    res.send(result);
+  });
 });
 
 /* Querying all data from Subject Table */
 app.get("/api/subject", (req, res) => {
-    const sqlSelect = "SELECT * FROM Subject;";
-    db.query(sqlSelect, (err, result) => {
-        res.send(result);
-    });
+  const sqlSelect = "SELECT * FROM Subject;";
+  db.query(sqlSelect, (err, result) => {
+    res.send(result);
+  });
 });
 
 /* Query sessions based on tutor id */
 app.get("/api/availableSessions", (req, res) => {
-    const currentTutor = req.query.tutorId;
-    console.log(currentTutor);
+  const currentTutor = req.query.tutorId;
+  console.log(currentTutor);
 
-    console.log("req = " + req);
-    const sqlGet = "SELECT * FROM Session WHERE tutorId = ?";
-    db.query(sqlGet, [currentTutor], (err, result) => {
-        res.send(result);
-    });
+  console.log("req = " + req);
+  const sqlGet = "SELECT * FROM Session WHERE tutorId = ?";
+  db.query(sqlGet, [currentTutor], (err, result) => {
+    res.send(result);
+  });
 });
 
 /* Query sessions for the subject */
 app.get("/api/sessionSelection", (req, res) => {
-    const currentSubject = req.query.selectedSubject;
-    console.log(currentSubject);
+  const currentSubject = req.query.selectedSubject;
+  console.log(currentSubject);
 
-    const sqlSelect = 
-        "SELECT * FROM Session WHERE subjectId = ?"
-        /*"JOIN Tutor ON Session.tutorId = Tutor.id"*/
-    db.query(sqlSelect, [currentSubject], (err, result) => {
-        res.send(result);
-    });
+  const sqlSelect = "SELECT * FROM Session WHERE subjectId = ?";
+  /*"JOIN Tutor ON Session.tutorId = Tutor.id"*/
+  db.query(sqlSelect, [currentSubject], (err, result) => {
+    res.send(result);
+  });
 });
 
 /* Querying all rooms from Room Table */
 app.get("/api/room", (req, res) => {
-    const sqlSelect = "SELECT * FROM Room;";
-    db.query(sqlSelect, (err, result) => {
-        res.send(result);
-    });
+  const sqlSelect = "SELECT * FROM Room;";
+  db.query(sqlSelect, (err, result) => {
+    res.send(result);
+  });
 });
 
-
-
 app.post("/api/insert", (req, res) => {
+  const firstName = req.body.firstName;
+  const lastName = req.body.lastName;
+  const emailAddress = req.body.emailAddress;
+  const password = req.body.password;
+  const classStanding = req.body.classStanding;
+  const role = req.body.role;
 
-    const firstName = req.body.firstName;
-    const lastName = req.body.lastName;
-    const emailAddress = req.body.emailAddress;
-    const password = req.body.password;
-    const classStanding = req.body.classStanding;
-    const role = req.body.role;
-
-    /* Determine if user signing up is a student or tutor */
-    if (role == "student") {
-        const sqlInsert = "INSERT INTO Student (firstName, lastName, classStanding, email, password) VALUES (?,?,?,?,?);"
-        db.query(sqlInsert, [firstName, lastName, classStanding, emailAddress, password], (err, result) => {
-            console.log(result);
-        });
-    } else if (role == "tutor") {
-        const sqlInsert = "INSERT INTO Tutor (password, firstName, lastName, classStanding, email) VALUES (?,?,?,?,?);"
-        db.query(sqlInsert, [password, firstName, lastName, classStanding, emailAddress], (err, result) => {
-            console.log(result);
-        });
-    }
+  /* Determine if user signing up is a student or tutor */
+  if (role == "student") {
+    const sqlInsert =
+      "INSERT INTO Student (firstName, lastName, classStanding, email, password) VALUES (?,?,?,?,?);";
+    db.query(
+      sqlInsert,
+      [firstName, lastName, classStanding, emailAddress, password],
+      (err, result) => {
+        console.log(result);
+      }
+    );
+  } else if (role == "tutor") {
+    const sqlInsert =
+      "INSERT INTO Tutor (password, firstName, lastName, classStanding, email) VALUES (?,?,?,?,?);";
+    db.query(
+      sqlInsert,
+      [password, firstName, lastName, classStanding, emailAddress],
+      (err, result) => {
+        console.log(result);
+      }
+    );
+  }
 });
 
 app.get("/test", (req, res) => {
   res.json({ message: "Hello from server!" });
 });
 
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
 });
 
 app.listen(PORT, () => {
-    console.log(`running on port ${PORT}`);
+  console.log(`running on port ${PORT}`);
 });
